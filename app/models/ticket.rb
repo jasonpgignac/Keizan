94ed6b1cc30e4c26d82ddbb3dfde9f28eb823c11
Jason Gignac <jasonpgignac@gmail.com>
2012-09-27 09:36:09 -0500

    Updated Ticket model to reduce errors

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,10 +6,10 @@ class Ticket < ActiveRecord::Base
   belongs_to :assignee, :class_name => "User"
   belongs_to :organization
   belongs_to :group
-  has_many :events
-  has_many :comments
-  has_many :satisfaction_ratings
-  has_many :value_changes
+  has_many :events, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :satisfaction_ratings, dependent: :destroy
+  has_many :value_changes, dependent: :destroy
   has_and_belongs_to_many :tags
  
   CUSTOM_FIELD_MAPS = {
@@ -55,66 +55,30 @@ class Ticket < ActiveRecord::Base
     end
   end
   
-  def self.create_from_zendesk_id(id, notify=true)
+  def self.create_from_zendesk_id(id, add_tags=true)
     ticket = Ticket.find(id) if Ticket.exists?(id)
     ticket ||= self.new
-    is_new = ticket.id.nil?
- 
-    data = cached_ticket_data_for(id)
-    # Directly Translated Fields
-    attrs = [
-      :id, 
-      :ticket_type, 
-      :subject, 
-      :priority, 
-      :status, 
-      :requestor_id, 
-      :submitter_id, 
-      :assignee_id, 
-      :organization_id, 
-      :group_id, 
-      :forum_topic_id, 
-      :problem_id, 
-      :has_incidents
-    ]
-    attrs.each do |attr|
-      ticket.update_attribute(attr, (data[attr.to_s]))
-    end
-    ticket.due_at = data["due_at"]
-    ticket.created_at = data["created_at"]
-    ticket.updated_at = data["updated_at"]
-    ticket.via = data["via"] ? data["via"]["channel"] : nil
+    ticket.id = id
     
-    # Custom Fields
-    data["fields"].each do |field_data|
-      ticket.update_attribute(CUSTOM_FIELD_MAPS[field_data["id"].to_i], field_data["value"]) if CUSTOM_FIELD_MAPS[field_data["id"].to_i]
-    end
-    ticket.notify_on_major_accounts if is_new && notify
-    ticket.mark_new_accounts if is_new
+    # Importing data about the ticket
+    ticket.import_fields_from_zendesk
+    ticket.create_associated_users_and_organization
     
-    if ticket.organization_id && !Organization.exists?(ticket.organization_id)
-      zo = CLIENT.organizations.find(ticket.organization_id)
-      ticket.organization = Organization.create_from_zendesk_object(zo)
-    end
+    # Adding new tags (if the ticket is new, and not closed)
+    if ticket.id.nil? && add_tags && !(ticket.status == "closed" || ticket.status == "Closed")
+      ticket.apply_watch_accounts
+      ticket.mark_new_accounts
+      ticket.assign_account_manager 
+    end 
     
-    # Tags
-    ticket.tags.destroy_all
-    ticket.tags = data["tags"].map { |ztag| (Tag.find_by_name(ztag) || Tag.create(:name => ztag)) }
-   
+    # Importing and Updating Tags
+    ticket.update_tags
+    
     ticket.save!
-    ticket.assign_account_manager if is_new && !(ticket.status == "closed" || ticket.status == "Closed")
     
-    # Events
-    audits = {}
-    ticket.cached_audits.each { |a| a["events"].each { |e| audits[a] ||= []; audits[a] << e}}
-    audits.each { |a,events| events.each { |e| Event.create_from_zendesk_objects(event: e, audit: a, ticket: ticket)}}
-    # Uncached Associated Objects
-    User.create_from_zendesk_object(CLIENT.users.find(ticket.requestor_id)) if ticket.requestor_id && !User.exists?(ticket.requestor_id)
-    User.create_from_zendesk_object(CLIENT.users.find(ticket.submitter_id)) if ticket.submitter_id && !User.exists?(ticket.submitter_id)
-    User.create_from_zendesk_object(CLIENT.users.find(ticket.assignee_id)) if ticket.assignee_id && !User.exists?(ticket.assignee_id)
-    Organization.create_from_zendesk_object(CLIENT.organizations.find(ticket.organization_id)) if ticket.organization_id && !Organization.exists?(ticket.organization_id)
-    Group.create_from_zendesk_object(CLIENT.groups.find(ticket.group_id)) if ticket.group_id && !Group.exists?(ticket.group_id)
-    
+    # Import audits 
+    ticket.import_audits
+        
     return ticket
   end
 
@@ -123,34 +87,45 @@ class Ticket < ActiveRecord::Base
     return organization.name.sub(/^D/, "")
   end
 
-  def notify_on_major_accounts
-    watch_accounts = WatchAccount.find_all_by_number(self.ddi)
-    unless watch_accounts.empty?
-      watch_accounts.each do |w|
-        if w.watch_account_type.notification_emails 
-          Pony.mail(
-            to:       w.watch_account_type.notification_emails, 
-            subject:  "[#{w.watch_account_type.name.upcase}] New Ticket for Account ##{self.ddi}",
-            body:     "There has been a new ticket for the account ##{self.ddi} (#{w.name})\n\n\nDate: #{self.created_at}\nTicket Number: #{self.id}\nTicket Subject: #{self.subject}\nURL: https://rackspacecloud.zendesk.com/tickets/#{self.id}"
-	  )
-	      end
-	      tag_array = tags.map { |t| t.name }
-	      default_tags = w.watch_account_type.default_tags
-        if (default_tags & tag_array) != default_tags
-          zt = CLIENT.tickets.find(self.id)
-          tags = zt.tags
-  	  tags = tags + default_tags
-          zt.tags = tags
-          zt.save
-          self.tags = self.tags + default_tags.map { |ztag| (Tag.find_by_name(ztag) || Tag.create(:name => ztag)) }
-	  self.save
-        end
+  def update_tags
+    if @zd_tags
+      #Update Keizan
+      self.tags.destroy_all
+      self.tags = @zd_tags.map { |ztag| (Tag.find_by_name(ztag) || Tag.create(:name => ztag)) }
+      
+      #Update Zendesk
+      if @zdtags_updated
+        zt = CLIENT.tickets.find(self.id)
+        zt.tags = (@zd_tags + zt.tags).uniq
+        zt.save
       end
+    end
+  end
+  
+  def apply_watch_accounts
+    import_fields_from_zendesk unless @zd_tags
+    
+    watch_accounts = WatchAccount.find_all_by_number(self.ddi)
+    watch_accounts.each do |w|
+      # First send notification emails 
+      Pony.mail(
+        to:       w.watch_account_type.notification_emails, 
+        subject:  "[#{w.watch_account_type.name.upcase}] New Ticket for Account ##{self.ddi}",
+        body:     "There has been a new ticket for the account ##{self.ddi} (#{w.name})\n\n\nDate: #{self.created_at}\nTicket Number: #{self.id}\nTicket Subject: #{self.subject}\nURL: https://rackspacecloud.zendesk.com/tickets/#{self.id}"
+      ) if w.watch_account_type.notification_emails
+	    
+	    if w.watch_account_type.default_tags
+  	    @zdtags = @zd_tags + w.watch_account_type.default_tags
+  	    @zdtags_updated = true
+  	  end
     end
   end
 
   def assign_account_manager
-    return nil unless self.tags.map { |tag| tag.name }.include?("futurezen")
+    import_fields_from_zendesk unless @zd_tags
+    
+    # We only round robin on 'futurezen' tickets
+    return nil unless @zd_tags.include?("futurezen")
     
     am_tags = [
       ["mc_sgilmore","steven.gilmore@rackspace.com"],
@@ -160,7 +135,7 @@ class Ticket < ActiveRecord::Base
       ["mc_ssanchez","seth.sanchez@rackspace.com"],
       ["mc_dbradley","daytona.bradley@RACKSPACE.COM"]
     ]
-    
+
     assigned_tags = am_tags.map {|t| t[0] } + [
       "cloud_uk",
       "smb_marquee",
@@ -174,30 +149,33 @@ class Ticket < ActiveRecord::Base
       "mc_sford"
     ]
     
+    # We only round robin on tickets that include none of the exception tags listed above
     return nil if assigned_tags - self.tags.map { |tag| tag.name } != assigned_tags
+    
+    #Here, we start the actual round robin assignment
+    raise RuntimeError, "Cannot assign round robin AM on ticket #{self.id} without an organization" unless organization
     
     redis = Redis.new
     
     unless redis.get("next_am_index")
       redis.set("next_am_index",0)
     end
-    Redis.new.lock_for_update("next_am_index") do
+    
+    redis.lock_for_update("next_am_index") do
       next_am_index = redis.get("next_am_index").to_i
-     
-      raise RuntimeError, "No Organization!" unless organization
       
       organization.with_lock do
         # Assign the next round robin am
         wat = WatchAccountType.where(name: am_tags[next_am_index][0]).first
-        wat ||= WatchAccountType.create(
-          name:         am_tags[next_am_index][0], 
-          default_tags: [ am_tags[next_am_index][0] ]
-        )
+        raise RuntimeError, "Trying to assign AM #{am_tags[next_am_index][0]}, but there is no matching WatchAccountType" unless wat
+        
         wa = WatchAccount.create(watch_account_type_id: wat.id, number: self.ddi)
-  	    self.notify_on_major_accounts()
+  	    @zd_tags = @zd_tags + w.watch_account_type.default_tags
           
         # increment next_am_index
         redis.set("next_am_index",(next_am_index + 1) % am_tags.size)
+        
+        # Mail notification
         Pony.mail(
           to:       ["jason.gignac@rackspace.com",am_tags[next_am_index][1]], 
           subject:  "[MANAGED] New Account for #{wat.name}",
@@ -219,21 +197,55 @@ class Ticket < ActiveRecord::Base
     end
   end
   
-  def self.cached_ticket_data_for(id)
-    #raw_data = REDIS.get("keizan__cache__ticket__#{id}")
-    #unless raw_data
-      raw_data = CLIENT.tickets.find(id).attributes["ticket"].to_json
-    #  REDIS.set("keizan__cache__ticket__#{id}",raw_data)
-    #  REDIS.set("keizan__cache__ticket__#{id}__audits",nil)
-    #end
-    return JSON.parse(raw_data)
+  def import_audits
+    audits = {}
+    zd_audits = JSON.parse(CLIENT.connection.send('get',"tickets/#{id}/audits.json").body["audits"].to_json)
+    zd_audits.each { |a| a["events"].each { |e| audits[a] ||= []; audits[a] << e}}
+    audits.each { |a,events| events.each { |e| Event.create_from_zendesk_objects(event: e, audit: a, ticket: self)}}
   end
-  def cached_audits
-    #raw_data = REDIS.get("keizan__cache__ticket__#{id}__audits")
-    #unless raw_data
-      raw_data = CLIENT.connection.send('get',"tickets/#{id}/audits.json").body["audits"].to_json
-      REDIS.set("keizan__cache__ticket__#{id}__audits",raw_data)
-    #end
-    return JSON.parse(raw_data)
+  
+  def import_fields_from_zendesk
+    data = JSON.parse(CLIENT.tickets.find(id).attributes["ticket"].to_json)
+    # Directly Translated Fields
+    attrs = [
+      :id, 
+      :ticket_type, 
+      :subject, 
+      :priority, 
+      :status, 
+      :requestor_id, 
+      :submitter_id, 
+      :assignee_id, 
+      :organization_id, 
+      :group_id, 
+      :forum_topic_id, 
+      :problem_id, 
+      :has_incidents
+    ]
+    attrs.each do |attr|
+      self.update_attribute(attr, (data[attr.to_s]))
+    end
+    
+    # Dates must be set by this method or the Zendesk plugin messes up the timezones
+    due_at = data["due_at"]
+    created_at = data["created_at"]
+    updated_at = data["updated_at"]
+    
+    # The only part of Via that we want is the channel
+    via = data["via"] ? data["via"]["channel"] : nil
+    
+    # Custom Fields
+    data["fields"].each do |field_data|
+      self.update_attribute(CUSTOM_FIELD_MAPS[field_data["id"].to_i], field_data["value"]) if CUSTOM_FIELD_MAPS[field_data["id"].to_i]
+    end
+    @zd_tags = data["tags"]
+  end
+  
+  def create_associated_users_and_organization
+    User.create_from_zendesk_object(CLIENT.users.find(self.requestor_id)) if self.requestor_id && !User.exists?(self.requestor_id)
+    User.create_from_zendesk_object(CLIENT.users.find(self.submitter_id)) if self.submitter_id && !User.exists?(self.submitter_id)
+    User.create_from_zendesk_object(CLIENT.users.find(self.assignee_id)) if self.assignee_id && !User.exists?(self.assignee_id)
+    Organization.create_from_zendesk_object(CLIENT.organizations.find(self.organization_id)) if self.organization_id && !Organization.exists?(self.organization_id)
+    Group.create_from_zendesk_object(CLIENT.groups.find(self.group_id)) if self.group_id && !Group.exists?(self.group_id)
   end
 end
