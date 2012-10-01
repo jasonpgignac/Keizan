7f69aacee6d7ee749883d5c5559a1a12e87493b5
Jason Gignac <jasonpgignac@gmail.com>
2012-10-01 11:12:09 -0500

    Added refresh value to TicketUpdater

--- app/jobs/ticket_updater.rb
+++ app/jobs/ticket_updater.rb
@@ -1,8 +1,10 @@
+
 class TicketUpdater
   include Resque::Plugins::UniqueJob
   @queue = "keizan_updater"
   
-  def self.perform(id)
+  def self.perform(id,refresh=false)
+    Ticket.find(id).destroy if refresh && Ticket.exists?(id)
     Ticket.create_from_zendesk_id(id)
   end
 end
